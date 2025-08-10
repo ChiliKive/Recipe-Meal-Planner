5ab84983ee6a2c9c29d582fb805ae563a0f2bfeb
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:10:20 +0300

    Add user foreign key to Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Recipe(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(S
