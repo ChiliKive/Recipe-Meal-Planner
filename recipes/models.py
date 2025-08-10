d33e7f68fb3ff87c3bc84e6ce33cf7539ef0e156
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:12:36 +0300

    No code changes made to the Recipe model.

--- recipes/models.py
+++ recipes/models.py
@@ -7,4 +7,5 @@ class Recipe(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     calories = models.IntegerField(null=True, blank=True)
-    image_path = models.ImageField(upload_to='recipes/', blank=True, null=True)
+    image_path = models.ImageField(upload_to='recipes/images/', blank=True, null=True)
+    
