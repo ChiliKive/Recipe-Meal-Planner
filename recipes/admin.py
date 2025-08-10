ae62b8e27c51b202a5513529ee160762bb16c387
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:46:00 +0300

    Register Recipe, Ingredient, and RecipeIngredient models in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from .models import Recipe, Ingredient, RecipeIngredient
 
-@admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    
+@admin.register(
