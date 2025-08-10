4f112ee07f7d1d8427dec3087040cf23be6277d3
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 15:10:25 +0300

    Maintain current structure and functionality without code changes.

--- mealplans/models.py
+++ mealplans/models.py
@@ -19,22 +19,6 @@ class Meal(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.name}"
     
-class MealPlan(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='meal_plans'
-    )
-    name = models.CharField(max_length=100)
-
-    description = models.TextField(blank=True, null=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-    def __str__(self):
-        return f"{self.name} for {self.user.username}"
 
 class MealPlanDay(models.Model):
     user = models.ForeignKey(
