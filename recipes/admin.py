0d78e13d29d7880ebcd7a77c793a5cc7c4905b53
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:46:10 +0300

    No code changes made to the Recipe, Ingredient, and RecipeIngredient models.

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Recipe, Ingredient, RecipeIngredient
 
-@admin.register(
+admin.site.register(Recipe)
+admin.site.register(Ingredient) 
+admin.site.register(RecipeIngredient)
