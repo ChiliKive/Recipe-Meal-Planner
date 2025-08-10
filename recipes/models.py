ddf3a8d92aa1089f729e7047bb0551653715b908
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:14:06 +0300

    No code changes made to the Recipe model.

--- recipes/models.py
+++ recipes/models.py
@@ -21,3 +21,7 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.title
+    
+class Ingredient(models.Model):
