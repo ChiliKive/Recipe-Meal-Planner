de1d757c934d59114c990f628d706b8f97d32c06
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:25:15 +0300

    No code changes made to the User and UserProfile models.

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,22 @@ class User(AbstractUser):
     # first_name
     # last_name
     # ...
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+    pass
     pass
 
 class UserProfile(models.Model):
