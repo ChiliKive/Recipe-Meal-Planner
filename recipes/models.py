56501bd197ca37df58827e70d39f7c7dfab8d156
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:12:56 +0300

    No code changes made to the Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -6,8 +6,12 @@ class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recipes')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    instructions = models.TextField(blank=True)
+    
     calories = models.IntegerField(null=True, blank=True)
+    
     image_path = models.ImageField(upload_to='recipes/images/', blank=True, null=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
