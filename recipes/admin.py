b41f1a919b8e43891ae4554d50e0af66dc00b634
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:45:50 +0300

    Add initial migration for Recipe, Ingredient, and RecipeIngredient models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Recipe, Ingredient, RecipeIngredient
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    
