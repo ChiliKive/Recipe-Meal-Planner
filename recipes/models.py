b0feecd83cac093d5d4653349afb6a897dcb0b9d
Nazar <ilchishin.nazar@gmail.com>
2025-08-10 14:11:43 +0300

    Fix incomplete calories field definition in Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -6,4 +6,4 @@ class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recipes')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    calories 
+    calories = models.
